931567e3132e2d684834d2e89231037edc9dbfb7
thushara-workspace-admin <thusharathilan123@gmail.com>
2025-04-12 17:22:55 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 import 'dotenv/config'
 import app from "./src/app.js";
+import connection from './src/config/database.config.js';
 
 const port = process.env.BACKEND_PORT
 const host = process.env.BACKEND_HOST
 
-app.listen(port, () => { console.log(`backend server runs on http://${host}:${port}`) })
+
+const server = async () => {
+    try {
+        await connection()
+        app.listen(port, () => { console.log(`backend server runs on http://${host}:${port}`) })
+    } catch (error) {
+
+    }
+}
+
+server()
