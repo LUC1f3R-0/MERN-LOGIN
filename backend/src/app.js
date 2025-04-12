dc5afe5454baba018d30b29ac46e74f01864fab2
thushara-workspace-admin <thusharathilan123@gmail.com>
2025-04-12 17:20:55 +0530

    app.js file updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import router from './routes/root.routes.js';
+import router from './routes/root.routes.js'
 const app = express()
 
 const host = process.env.FRONTEND_HOST
